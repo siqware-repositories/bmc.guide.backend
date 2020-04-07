5e7e839684fce4c671eabd5bef10af178744ff05
tunchamroeun <chamroeuntun25@outlook.com>
2020-04-07 09:18:26 +0700

    bmc.guide.backend update image

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -41,8 +41,8 @@ class TravelController extends Controller
         $gallery->description = 'Travel Description';
         $gallery->save();
         foreach ($galleries as $file){
-            $img = Image::make($file)->encode('png',100);
-            $name = uniqid().'-'.time() . '.png';
+            $img = Image::make($file)->encode('jpg',75);
+            $name = uniqid().'-'.time() . '.jpg';
             Storage::disk('public')->put($name, $img);
             DB::table('gallery_details')->insert([
                 'gallery_id'=>$gallery->id,
@@ -51,8 +51,8 @@ class TravelController extends Controller
                 'updated_at'=>Carbon::now(),
             ]);
         }
-        $img = Image::make($thumbnail)->encode('png',100);
-        $name = uniqid().'-'.time() . '.png';
+        $img = Image::make($thumbnail)->encode('jpg',75);
+        $name = uniqid().'-'.time() . '.jpg';
         Storage::disk('public')->put($name, $img);
 
         $store = new Travel();
@@ -88,8 +88,8 @@ class TravelController extends Controller
         $travel = Travel::findOrFail($id);
         $travel->title = $validData['title'];
         if (isset($input['thumbnail'])){
-            $img = Image::make($thumbnail)->encode('png',100);
-            $name = uniqid().'-'.time() . '.png';
+            $img = Image::make($thumbnail)->encode('jpg',75);
+            $name = uniqid().'-'.time() . '.jpg';
             Storage::disk('public')->put($name, $img);
             $travel->thumbnail = url(Storage::url($name));
         }
@@ -100,8 +100,8 @@ class TravelController extends Controller
         $travel->save();
         if (isset($input['galleries'])) {
             foreach ($galleries as $file) {
-                $img = Image::make($file)->encode('png', 100);
-                $name = uniqid() . '-' . time() . '.png';
+                $img = Image::make($file)->encode('jpg', 100);
+                $name = uniqid() . '-' . time() . '.jpg';
                 Storage::disk('public')->put($name, $img);
                 DB::table('gallery_details')->insert([
                     'gallery_id' => $travel->gallery_id,
